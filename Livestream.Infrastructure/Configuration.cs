01e0bee55c224f5e444303faf0638f319a12ad79
Steijn Ploegmakers <461991@student.fontys.nl>
2024-12-03 23:57:55 +0100

    docs: added documenting of infra configuration

--- Livestream.Infrastructure/Configuration.cs
+++ Livestream.Infrastructure/Configuration.cs
@@ -3,8 +3,16 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Livestream.Infrastructure
 {
+    /// <summary>
+    /// Startup extensions
+    /// </summary>
     public static class Configuration
     {
+        /// <summary>
+        /// Add infrastructure layer
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
         public static IServiceCollection AddInfrastructure(this IServiceCollection services)
         {
             services.AddSingleton<IEventPublisher, EventPublisher>();
