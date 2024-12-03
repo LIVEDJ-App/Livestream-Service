58c588d7b55b2a455a3b605ffc8854c14668dde7
Steijn Ploegmakers <461991@student.fontys.nl>
2024-12-03 21:53:55 +0100

    feat: created sonar-scan script

---
+++ sonar-scan.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+SONAR_PROJECT="LIVEDJ-App_Livestream-Service"
+SONARCLOUD_ORGANIZATION_NAME="livedj-app"
+SONAR_TOKEN="$1"
+
+dotnet sonarscanner begin \
+  /k:"${SONAR_PROJECT}" \
+  /d:"sonar.login=${{secrets.SONAR_TOKEN}}" \
+  /o:"${SONARCLOUD_ORGANIZATION_NAME}" \
+  /d:"sonar.host.url=https://sonarcloud.io" \
+  /d:sonar.cs.vscoveragexml.reportsPaths=coverage.xml \
+  /d:sonar.coverage.exclusions="\
+    **/Livestream.Domain,\
+    **/Livestream.Infrastructure/**,\
+    **/Livestream.Persistence,\
+    **/*Configuration.cs,\
