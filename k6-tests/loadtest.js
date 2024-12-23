de3b0f2711f2397c2c25a1560defffc50336a6a5
Steijn Ploegmakers <461991@student.fontys.nl>
2024-12-23 19:05:35 +0100

    fix: updated load tests

--- k6-tests/loadtest.js
+++ k6-tests/loadtest.js
@@ -2,8 +2,8 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-  vus: 34,
-  duration: '30s',
+  vus: 1000,
+  duration: '60s',
   cloud: {
     // LIVEDJ-Project
     projectID: 3727684,
