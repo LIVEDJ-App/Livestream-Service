3524696f37b8e4b7c0e48d33f586422cbaeb830c
Steijn Ploegmakers <461991@student.fontys.nl>
2024-12-23 19:09:06 +0100

    fix: changed vus based on max

--- k6-tests/loadtest.js
+++ k6-tests/loadtest.js
@@ -2,8 +2,8 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-  vus: 1000,
-  duration: '60s',
+  vus: 100,
+  duration: '5m',
   cloud: {
     // LIVEDJ-Project
     projectID: 3727684,
