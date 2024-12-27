cebd5dd5da1629af08d9e5112a5e75830e99068c
Steijn Ploegmakers <461991@student.fontys.nl>
2024-12-27 21:45:14 +0100

    fix: fix code smell

--- Livestream.Infrastructure/Metrics/LivestreamMetrics.cs
+++ Livestream.Infrastructure/Metrics/LivestreamMetrics.cs
@@ -4,7 +4,7 @@ using System.Diagnostics.Metrics;
 
 namespace Livestream.Infrastructure.Metrics
 {
-    public class LivestreamMetrics
+    public static class LivestreamMetrics
     {
         public static readonly Meter Meter = new("Livestream.Metrics");
     public static readonly Counter<int> LivestreamsCreated = Meter.CreateCounter<int>("products_created");
